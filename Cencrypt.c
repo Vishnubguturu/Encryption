85a0a8546c93d3f3c714840fb78ec0857f1ca147
Vishnu Babu Guturu <122582931+VishnuBabu17@users.noreply.github.com>
2026-05-18 04:29:38 -0700

    Initial commit - Encryption

--- Cencrypt.c
+++ Cencrypt.c
@@ -10,6 +10,18 @@ encrypt(char *iobuf, char *bookbuf, int cnt)
 {
 
     // your code here
+    if(cnt < 0){
+        return 0;
+    }
+
+    size_t input_byte = 0;
+    size_t book_byte = 0;
+
+    for(int i = 0; i < cnt; i++){
+        input_byte = iobuf[i];
+        book_byte = bookbuf[i];
+        iobuf[i] = input_byte ^ book_byte;
+    }
 
     return cnt;
 }
--- SELVERS.h
+++ SELVERS.h
@@ -7,7 +7,7 @@
 // if both are commented out, use the solution code
 //
 //#define MYENCRYPT_C       // when defined will use your encrypt.c for Part 1
-//#define MYENCRYPT_S       // when defined will use your encrypt.S for Part 1 
+#define MYENCRYPT_S       // when defined will use your encrypt.S for Part 1 
 
 #if defined MYENCRYPT_S  && defined MYENCRYPT_C
 #error "You can only have one of MYENCRYPT_S and MYENCRYPT_C defined"
@@ -16,8 +16,8 @@
 //
 // only one of the following two must be uncommented
 //
-#define MAIN_C            // Must be uncommented to use Cmain.c for Part 1
-//#define MYMAIN_S          // when defined will use your main.S for Part 2
+//#define MAIN_C            // Must be uncommented to use Cmain.c for Part 1
+#define MYMAIN_S          // when defined will use your main.S for Part 2
 
 #if defined MYMAIN_S  && defined MAIN_C
 #error "You can only have one of MYMAIN_S and MAIN_C defined"
@@ -29,8 +29,8 @@
 //
 // only one of the following two must be uncommented
 //
-#define RDBUF_C           // Must be uncommented to use Crdbuf.c for Part 1
-//#define MYRDBUF_S         // when defined will use your rdbuf.S for Part 2
+//#define RDBUF_C           // Must be uncommented to use Crdbuf.c for Part 1
+#define MYRDBUF_S         // when defined will use your rdbuf.S for Part 2
 
 #if defined MYRDBUF_S  && defined RDBUF_C
 #error "You can only have one of MYRDBUF_S and RDBUF_C defined"
